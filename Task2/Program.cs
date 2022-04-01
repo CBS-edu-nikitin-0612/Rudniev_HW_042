030b5291581f3862f4eecee1a7e9988c6c4e65e4
Vladyslav Rudniev <Rudniev.CSdev@gmail.com>
2022-04-01 12:04:31 +0300

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, int, double[]> asyncMethod = new Func<double, int, double[]>(Pow);
+            IAsyncResult asyncResult = asyncMethod.BeginInvoke(2, 10, CallBackMethod, "Async method ic completed!");
+            double[] result = asyncMethod.EndInvoke(asyncResult);
+            foreach (var item in result)
+            {
+                Console.Write($"{item}  ");
+            }
+            Console.ReadKey();
+        }
+        static double[] Pow(double number, int degree)
+        {
+            double[] doubleArray = new double[degree];
+            if (degree < 1)
+                return null;
+
+            for (int i = 1; i <= degree; i++)
+            {
+                doubleArray[i - 1] = Math.Pow(number, i);
+            }
+            return doubleArray;
+        }
+        static void CallBackMethod(IAsyncResult asyncResult)
+        {
+            if (asyncResult.IsCompleted)
+                Console.WriteLine(asyncResult.AsyncState);
+            else
+                Console.WriteLine("Attention!");
+        }
+    }
+}
---
+++ aditionalTask/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aditionalTask
+{
+    public partial class Form1 : Form
+    {
+        Func<bool> myDel;
+        IAsyncResult asyncResult;
+        public Form1()
+        {
+            InitializeComponent();
+            myDel = new Func<bool>(OpenImage);
+        }
+
+        private bool OpenImage()
+        {
+            try
+            {
+                this.pictureBox.Image = Properties.Resources.Hello_World;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            
+        }
+        private void CallbackMethod(IAsyncResult asyncResult)
+        {
+            if (asyncResult.IsCompleted)
+                label1.Text = "result: completed!";
+            else
+                label1.Text = "result: not completed!";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            asyncResult = myDel.BeginInvoke(null, null);
+            asyncResult.AsyncWaitHandle.WaitOne();
+            if (asyncResult.IsCompleted)
+                label1.Text = "result: completed!";
+            else
+                label1.Text = "result: not completed!";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            asyncResult = myDel.BeginInvoke(null, null);
+            bool result = myDel.EndInvoke(asyncResult);
+            if (result)
+                label1.Text = "result: successfully!";
+            else
+                label1.Text = "result: failed!";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            asyncResult = myDel.BeginInvoke(CallbackMethod, null);
+        }
+    }
+}
